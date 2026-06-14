b4d6165ddd6babcc803d3ff05095bee234f7d32c
dell-ubuntu <dell-ubuntu@example.com>
2026-06-14 17:34:21 -0500

    Updated process_check.pl script

--- Perl_Cookbook/process_check.pl
+++ Perl_Cookbook/process_check.pl
@@ -5,46 +5,55 @@
 use strict;
 use warnings;
 use Time::Local;
+use autodie;
 
 my $filename = 'process_log.txt';
 my $delimiter = '|';
 
-open (my $fh, '<', 'process_log.txt') or die "Can't open '$filename': $!";
+open (my $fh, '<', 'process_log.txt');
 
-while (my $line = <$fh>) {
+#Slurp the entire file into an array at once
+my @all_lines = <$fh>;
+close $fh;
+
+#Use grep to filter out the header
+my @filtered_lines = grep { !/^Process_ID/ } @all_lines;
+
+foreach my $line (@filtered_lines) {
 	chomp($line);
-	next if $line =~ /^Process_ID/;		#Skip header line
 
-	#Split the line into an array based on the delimiter
-	
-	my ($pid, $start_string, $current_string) = split(/\|/, $line);
+	#Split the line into an array based on the delimiter variable
+	my ($pid, $start_string, $current_string) = split(/\Q$delimiter\E/, $line);
+
+	#Use /r to isolate the time portion cleanly for potential future use without breaking the orginal string
+	my $start_time_only = $start_string =~ s/^\d{4}-\d{2}-\d{2}\s+//r;
 
 	#Extract integers using regex groups
 	
 	my ($s_yyyy, $s_mm, $s_dd, $s_hours, $s_minutes, $s_seconds) =
-
 	($start_string =~ /(\d+)-(\d+)-(\d+)\s+(\d+):(\d+):(\d+)/);
 
 	my ($c_yyyy, $c_mm, $c_dd, $c_hours, $c_minutes, $c_seconds) =
-
 	($current_string =~ /(\d+)-(\d+)-(\d+)\s+(\d+):(\d+):(\d+)/);
 
 	#Convert to Epoch Seconds
-	
 	my $start_epoch = timelocal($s_seconds, $s_minutes, $s_hours, $s_dd, $s_mm -1, $s_yyyy);
-
 	my $current_epoch = timelocal($c_seconds, $c_minutes, $c_hours, $c_dd, $c_mm -1, $c_yyyy);
 
 	#Calculate the Delta
-	
 	my $diff_seconds = $current_epoch - $start_epoch;
-	my $diff_minutes = $diff_seconds/60;
+	my $diff_minutes = $diff_seconds / 60;
 
-	#Check Threshold
+	#Check Threshold and Print Clean Output
 	
 	if ($diff_minutes > 30) {
-		print "CRITICAL ALERT: Process $pid has been running for $diff_minutes minutes.\n";
+		
+		#Calculate clean hours and remaining minutes
+		my $hours_running = int($diff_minutes / 60);
+		my $minutes_running = $diff_minutes % 60;
+
+		my $time_string = $hours_running > 0 ? "${hours_running}h ${minutes_running}m" : "${minutes_running}m";
+		
+		print "CRITICAL ALERT: Process $pid has been running for $time_string.\n";
 	}
 }
-
-close $fh;
